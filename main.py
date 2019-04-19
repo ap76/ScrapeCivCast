83f0750e36cdf9a21163ad1f69ba477d65f0972c
ap76 <ap.76@icloud.com>
2019-04-19 13:42:05 -0500

    everything runs fine

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
+import time
+import openpyxl
 
+driver = webdriver.Firefox()
+driver.implicitly_wait(30)
 
-url = "http://www.ijreview.com/wp-admin/admin-ajax.php"
-session = requests.Session()
-page_size = 24
+wb = openpyxl.Workbook()
+row_excel = 0
 
+with open("list", "r") as list_file:
+    for url in list_file:
+        row_excel += 1
+        print(row_excel)
+        print(url)
+        driver.get(url)
+        time.sleep(3)
+        outerHTML = driver.execute_script("return document.documentElement.outerHTML")
+        html_soup = BeautifulSoup(outerHTML, 'html.parser')
+        #url sent to the Excel file
+        wb.active.cell(row=row_excel, column=1).value = str(url)
 
-params = {
-    'action': 'load_more',
-    'numPosts': page_size,
-    'category': '',
-    'orderby': 'date',
-    'time': ''
-}
+        # Project scope sent to the Excel file
+        project_scope = []
+        for i in html_soup.find_all('div', class_='card-body'):
+            project_scope.append(i.text)
+        # for i in range(len(project_scope)):
+        # print(project_scope[4])
+        if len(project_scope)>1:
+            wb.active.cell(row=row_excel, column=2).value = str(project_scope[4]).strip()
 
-offset = 0
-limit = 100
-while offset < limit:
-    params['offset'] = offset
-    response = session.post(url, data=params)
-    links = [a['href'] for a in BeautifulSoup(response.content).select('li > a')]
-    for link in links:
-        response = session.get(link)
-        page = BeautifulSoup(response.content)
-        title = page.find('title').text.strip()
-        author = page.find('span', class_='author').text.strip()
-        print {'link': link, 'title': title, 'author': author}
-
-    offset += page_size
+        # Project description sent to the Excel file
+        cc = []
+        for i in html_soup.find_all('div', class_='col-8 col-md-7 col-xl-8'):
+            # print (i.text)
+            cc.append(i.text)
+        for i in range(len(cc)):
+            # print(cc[i])
+            wb.active.cell(row=row_excel, column=i + 3).value = str(cc[i]).strip()
+        wb.save('scraped.xlsx')
+wb.close()
